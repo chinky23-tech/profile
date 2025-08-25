214c11e440dead169364e0df4f33a752573acac9
chinky23-tech <chinky0330@gmail.com>
2025-08-25 23:32:43 +0530

    animation

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-document.getElementById('myButton').addEventListener('click',function(){
-alert('button clicked');
+//fade and scale in when page loads
+
+gsap.from("#card",{
+    opacity:0,
+    y:50,
+    scale:0.8,
+    duration:1,
+    ease:"power3.out"
 });
 
+//hover animation
+const card = document.getElementById("card");
+card.addEventListener("mouseenter",()=>{
+    gsap.to(card,{ scale: 1.05,duration: 0.3, ease: "power2.out"});
+});
+card.addEventListener("mouseleave",() =>{
+    gsap.to(card,{ scale: 1,duration:0.3, ease: "power2.out"});
+});
+// click animation (bounce effect)
+
+card.addEventListener("click",() => {
+gsap.to(card,{scale: 0.95, duration: 0.1, yoyo: true, repeat: 1});
+});
